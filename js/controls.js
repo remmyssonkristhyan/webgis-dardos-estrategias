b4b1b0063b4a3391faa14c5dae9fac8bf0dfa2e5
Remmysson Kristhyan <rkristhyan@gmail.com>
2023-01-23 02:03:07 -0300

    add mouse position and scale controls

---
+++ js/controls.js
@@ -0,0 +1,19 @@
+
+import './../css/controls.css'
+
+
+// mousePosition
+
+export const mousePosition = new ol.control.MousePosition({
+    coordinateFormat: ol.coordinate.createStringXY(4),
+    projection: 'EPSG:4326',
+    className: 'mousePosition'
+});
+  
+  
+// scaleControl  
+  
+export const scaleControl = new ol.control.ScaleLine({
+    bar: true,
+    text: true
+});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import './../libs/v6.4.3-dist/ol.css'
-import './../css/style.css'
+import './../libs/v6.4.3-dist/ol.css';
+import './../css/style.css';
+import * as Controls from './controls.js';
 
 const apiKey = import.meta.env.HERE_API_KEY;
 
@@ -65,11 +66,17 @@ window.onload = init
 
 function init() {
   const map = new ol.Map({
-      view: new ol.View({
-          center: ol.proj.fromLonLat([-35.7356, -9.66512]),
-          zoom: 10
-      }),
-      layers: layers,
-      target: 'map'
-    })
+    view: new ol.View({
+      center: ol.proj.fromLonLat([-35.7356, -9.66512]),
+      zoom: 10
+    }),
+    layers: layers,
+    target: 'map'
+  })
+
+  // Add controls
+
+  map.addControl(Controls.mousePosition)
+  map.addControl(Controls.scaleControl);
+
 }
